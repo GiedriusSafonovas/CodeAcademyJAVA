8f1d23b1329d17eb4cb5be901aa1797b65ebf523
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-21 12:54:27 +0200

    Done Task1

---
+++ nd1/src/main/java/lt/codeacademy/Task1.java
@@ -0,0 +1,13 @@
+package lt.codeacademy;
+
+public class Task1 {
+    public static void main(String[] args) {
+        printText("Vienas");
+        String tekstas = "Du";
+        printText(tekstas);
+    }
+
+    public static void printText(String text){
+        System.out.println(text);
+    }
+}
