06418766d3cbd4f51aedae2ddc2a4373022ba4b2
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-21 13:05:32 +0200

    Task4 i.

---
+++ nd1/src/main/java/lt/codeacademy/Task4.java
@@ -0,0 +1,21 @@
+package lt.codeacademy;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Iveskite sakini");
+//        String text = sc.nextLine();
+        String text = "Vienas, du, trys";
+
+        System.out.println("---------i---------");
+        System.out.println(text.toUpperCase());
+
+        System.out.println("---------ii--------");
+        System.out.println("---------iii-------");
+        System.out.println("---------iv--------");
+        System.out.println("---------v---------");
+        System.out.println("---------vi--------");
+    }
+}
