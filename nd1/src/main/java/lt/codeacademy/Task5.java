21efd4c40ea029361bf72fa168b6ecffbd6754a2
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-21 13:21:21 +0200

    Task 5 a. dalis

---
+++ nd1/src/main/java/lt/codeacademy/Task5.java
@@ -0,0 +1,16 @@
+package lt.codeacademy;
+
+public class Task5 {
+    public static void main(String[] args) {
+        String[] menesiai = {"Sausis", "Vasaris", "Kovas", "Balandis",
+                "Geguze", "Birzelis", "Liepa", "Rugpjutis",
+                "Rugsejis", "Spalis", "Lapkritis", "Gruodis"};
+
+        System.out.println("Masyvo dydis: " + menesiai.length);
+        System.out.println("Masyvo pirma reiksme: " + menesiai[0]);
+        System.out.println("Masyvo paskutine reiksme " + menesiai[menesiai.length-1]);
+    }
+
+
+
+}
