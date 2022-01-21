2a14fca2d77ce812ffc7eeb6c4ed8cf2896689e9
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-21 13:23:25 +0200

    Task 5 b. dalis

--- nd1/src/main/java/lt/codeacademy/Task5.java
+++ nd1/src/main/java/lt/codeacademy/Task5.java
@@ -1,5 +1,7 @@
 package lt.codeacademy;
 
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
         String[] menesiai = {"Sausis", "Vasaris", "Kovas", "Balandis",
@@ -9,6 +11,13 @@ public class Task5 {
         System.out.println("Masyvo dydis: " + menesiai.length);
         System.out.println("Masyvo pirma reiksme: " + menesiai[0]);
         System.out.println("Masyvo paskutine reiksme " + menesiai[menesiai.length-1]);
+
+        System.out.println("************************");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite menesio nr. [1-12]");
+        int sk = sc.nextInt();
+        System.out.println(menesiai[sk-1]);
     }
 
 
