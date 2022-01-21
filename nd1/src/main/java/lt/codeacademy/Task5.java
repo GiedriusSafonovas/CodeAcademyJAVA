f1c6d065b170ad7ff6dca101a7357b150bed2f01
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-21 13:32:40 +0200

    Task 5 c. dalis

--- nd1/src/main/java/lt/codeacademy/Task5.java
+++ nd1/src/main/java/lt/codeacademy/Task5.java
@@ -9,15 +9,34 @@ public class Task5 {
                 "Rugsejis", "Spalis", "Lapkritis", "Gruodis"};
 
         System.out.println("Masyvo dydis: " + menesiai.length);
-        System.out.println("Masyvo pirma reiksme: " + menesiai[0]);
-        System.out.println("Masyvo paskutine reiksme " + menesiai[menesiai.length-1]);
+        String pirmasMenuo = menesiai[0];
+        System.out.println("Masyvo pirma reiksme: " + pirmasMenuo);
+        String paskutinisMenuo = menesiai[menesiai.length-1];
+        System.out.println("Masyvo paskutine reiksme " + paskutinisMenuo);
 
         System.out.println("************************");
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Iveskite menesio nr. [1-12]");
         int sk = sc.nextInt();
-        System.out.println(menesiai[sk-1]);
+        String pasirinktasMenuo = menesiai[sk-1];
+        String menuoPo;
+        String menuoPries;
+        if(sk == 1){
+            menuoPries = paskutinisMenuo;
+        }else{
+            menuoPries = menesiai[sk-2];
+        }
+        if(sk == 12){
+            menuoPo = pirmasMenuo;
+        }else{
+            menuoPo = menesiai[sk];
+        }
+        System.out.println("Pasirinktas menuo: " + pasirinktasMenuo);
+        System.out.println("Menuo pries pasirinkta: " + menuoPries);
+        System.out.println("Menuo po pasirinkto: " + menuoPo);
+
+
     }
 
 
