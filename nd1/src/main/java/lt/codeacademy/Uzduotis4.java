37d61ef59fd40b809b16e5f337ba92ccb9a11f37
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-22 13:23:52 +0200

    Operatoriai: Uzduotis 4, 1 dalis

---
+++ nd1/src/main/java/lt/codeacademy/Uzduotis4.java
@@ -0,0 +1,19 @@
+package lt.codeacademy;
+
+public class Uzduotis4 {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = true;
+        boolean b3 = true;
+        boolean b4 = false;
+        boolean b5 = false;
+        boolean b6 = false;
+
+        if(b1) System.out.println("b1");
+        if(b2) System.out.println("b2");
+        if(b3) System.out.println("b3");
+        if(b4) System.out.println("b4");
+        if(b5) System.out.println("b5");
+        if(b6) System.out.println("b6");
+    }
+}
