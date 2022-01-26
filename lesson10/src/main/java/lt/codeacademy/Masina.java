2301ffd223bc6ba39d6250bacf3a50d0b564cb3e
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-01-26 20:34:22 +0200

    Pirmoje klaseje sukurtas kitos klases objektas ir pabantyta pasiekti kintamuosius ir metodus

--- lesson10/src/main/java/lt/codeacademy/Masina.java
+++ lesson10/src/main/java/lt/codeacademy/Masina.java
@@ -1,6 +1,14 @@
 package lt.codeacademy;
 
 public class Masina {
+        Ratas ratas;
+    {
+        ratas = new Ratas();
+        protectedMasinaLaukas1 = ratas.protectedRatasLaukas1;
+        defaultMasinaLaukas1 = ratas.defaultRatasLaukas1;
+        publicMasinaLaukas1 = ratas.publicRatasLaukas1;
+    }
+
     protected String protectedMasinaLaukas1;
     protected String protectedMasinaLaukas2;
 
@@ -10,13 +18,19 @@ public class Masina {
     public String publicMasinaLaukas1;
     public String publicMasinaLaukas2;
 
-    protected void protectedMasinaMetodas1(){}
+    protected void protectedMasinaMetodas1(){
+        ratas.protectedRatasMetodas1();
+    }
     protected void protectedMasinaMetodas2(){}
 
-    void defaultMasinaMetodas1(){}
+    void defaultMasinaMetodas1(){
+        ratas.defaultRatasMetodas1();
+    }
     void defaultMasinaMetodas2(){}
 
-    public void publicMasinaMetodas1(){}
+    public void publicMasinaMetodas1(){
+        ratas.publicRatasMetodas1();
+    }
     public void publicMasinaMetodas2(){}
 
 }
