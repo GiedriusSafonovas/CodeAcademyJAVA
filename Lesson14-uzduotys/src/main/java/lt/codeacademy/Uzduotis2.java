dca42eb8be22117de8fba288ba54e33f53a55ce8
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-02-02 19:26:46 +0200

    String format uzduotis

---
+++ Lesson14-uzduotys/src/main/java/lt/codeacademy/Uzduotis2.java
@@ -0,0 +1,8 @@
+package lt.codeacademy;
+
+public class Uzduotis2 {
+    public static void printText(){
+        String tekstas = String.format("|%20s|\n|%20s|\n|%20s|\n|%20s|\n|%-20s|\n","Aš","tikrai","išmoksiu","programuoti","Java");
+        System.out.println(tekstas);
+    }
+}
