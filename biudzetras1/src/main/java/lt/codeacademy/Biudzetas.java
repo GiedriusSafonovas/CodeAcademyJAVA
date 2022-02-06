00ea7e60afd10f56f1735d075a337fde1232d490
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-02-06 13:57:22 +0200

    Sukurti metodai prideti irasams

--- biudzetras1/src/main/java/lt/codeacademy/Biudzetas.java
+++ biudzetras1/src/main/java/lt/codeacademy/Biudzetas.java
@@ -3,4 +3,24 @@ package lt.codeacademy;
 public class Biudzetas {
   PajamuIrasas[] pajamos = new PajamuIrasas[100];
   IslaiduIrasas[] islaidos = new IslaiduIrasas[100];
+
+  public void pridetiPajamuIrasa(PajamuIrasas pajamuIrasas){
+    for(PajamuIrasas irasas : pajamos){
+      if(irasas == null){
+        irasas = pajamuIrasas;
+        break;
+      }
+    }
+  }
+
+  public void pridetiIslaiduIrasa(IslaiduIrasas islaiduIrasas){
+    for(IslaiduIrasas irasas : islaidos){
+      if(irasas == null){
+        irasas = islaiduIrasas;
+        break;
+      }
+    }
+  }
+
+
 }
