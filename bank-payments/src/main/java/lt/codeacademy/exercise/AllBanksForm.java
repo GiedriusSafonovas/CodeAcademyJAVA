74e1970ace9578dbd0647cfcdb2050d6fa07e854
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-02-10 19:41:59 +0200

    Pakeistas mergeForms metodas AllBanksForm klaseje, kad butu maziau dubliavimosi

--- bank-payments/src/main/java/lt/codeacademy/exercise/AllBanksForm.java
+++ bank-payments/src/main/java/lt/codeacademy/exercise/AllBanksForm.java
@@ -1,34 +1,23 @@
 package lt.codeacademy.exercise;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class AllBanksForm extends Form {
-  LumiForm lumi;
-  SepForm sep;
-  ShvedForm shved;
+  private ArrayList<Form> banks;
 
   public AllBanksForm() {
-    this.lumi = new LumiForm();
-    this.sep = new SepForm();
-    this.shved = new ShvedForm();
-
+    banks = new ArrayList<>();
+    Collections.addAll(banks, new LumiForm(), new SepForm(), new ShvedForm());
     mergeForms();
   }
 
-  public void mergeForms(){
-    this.getName().addAll(lumi.getName());
-    this.getName().addAll(sep.getName());
-    this.getName().addAll(shved.getName());
-
-    this.getDate().addAll(lumi.getDate());
-    this.getDate().addAll(sep.getDate());
-    this.getDate().addAll(shved.getDate());
-
-    this.getIban().addAll(lumi.getIban());
-    this.getIban().addAll(sep.getIban());
-    this.getIban().addAll(shved.getIban());
-
-    this.getAmount().addAll(lumi.getAmount());
-    this.getAmount().addAll(sep.getAmount());
-    this.getAmount().addAll(shved.getAmount());
+  public void mergeForms() {
+    for (Form bank : banks) {
+      this.getName().addAll(bank.getName());
+      this.getDate().addAll(bank.getDate());
+      this.getIban().addAll(bank.getIban());
+      this.getAmount().addAll(bank.getAmount());
+    }
   }
-
 }
