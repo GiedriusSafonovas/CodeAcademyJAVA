deeea302997a2b619b190e54346d5cbfe9df0706
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-03-01 13:21:16 +0200

    balanso testas

--- biudzetras1/src/test/java/lt/codeacademy/BiudzetasTest.java
+++ biudzetras1/src/test/java/lt/codeacademy/BiudzetasTest.java
@@ -3,42 +3,23 @@ package lt.codeacademy;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class BiudzetasTest {
-  Biudzetas biudzetas = new Biudzetas();
-//  PajamuIrasas pajamos = new PajamuIrasas(21.5f, "2022-02-06", "Pardavimai", true, "nera");
-//  IslaiduIrasas islaidos =
-//      new IslaiduIrasas(10, "2022-02-06 12:30", "Pirkimai", "Banko " + "pavedimas", "nera");
 
-//  @Test
-//  public void gautiPajamuIrasaTest() {
-//    biudzetas.pridetiPajamuIrasa(pajamos);
-//    PajamuIrasas result = biudzetas.gautiPajamuIrasa("Pardavimai", "2022-02-06");
-//    Assert.assertEquals(pajamos, result);
-//  }
-//
-//  @Test
-//  public void gautiIslaiduIrasaTest() {
-//    biudzetas.pridetiIslaiduIrasa(islaidos);
-//    IslaiduIrasas result = biudzetas.gautiIslaiduIrasa("Pirkimai", "2022-02-06 12:30");
-//    Assert.assertEquals(islaidos, result);
-//  }
+  @Test
+  public void balansasTest(){
+    Biudzetas biudzetas = new Biudzetas();
+    LocalDate data = LocalDate.now();
+    LocalDateTime dataSuLaiku = LocalDateTime.now();
+    PajamuIrasas pajamos = new PajamuIrasas(20.5F,data,"Pardavimai", true, "nera");
+    IslaiduIrasas islaidos = new IslaiduIrasas(10.5F,dataSuLaiku,"Pirkimai", "kortele", "nera");
+    biudzetas.pridetiIrasa(pajamos);
+    biudzetas.pridetiIrasa(islaidos);
 
-//  @Test
-//  public void balansasTest() {
-//    biudzetas.pridetiIrasa(pajamos);
-//    biudzetas.pridetiIrasa(islaidos);
-//    float result = biudzetas.balansas();
-//    float expected = 21.5f - 10;
-//    Assert.assertEquals(expected, result, 0);
-//  }
-
-//  @Test
-//  public void trintiIrasaTest(){
-//    Biudzetas b = new Biudzetas();
-//    b.pridetiIrasa(pajamos);
-//    b.trintiIrasa("P1");
-//    Object result = biudzetas.gautiPajamuIrasa("Pardavimai", "2022-02-06");
-//    Assert.assertNull(result);
-//  }
+    float result = biudzetas.balansas();
+    Assert.assertEquals(10.0F,result,0);
+  }
 
 }
