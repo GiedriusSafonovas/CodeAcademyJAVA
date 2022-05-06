1ee2af076674260776d35dd0caa1d5f7a8e98cf6
DESKTOP-OPM29B3\G <g.safonovas@gmail.com>
2022-05-06 12:55:52 +0300

    add postgres

--- Thymeleaf projektas/src/main/java/lt/codeacademy/thymeleafprojektas/model/Song.java
+++ Thymeleaf projektas/src/main/java/lt/codeacademy/thymeleafprojektas/model/Song.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 @Getter
 @Setter
 @RequiredArgsConstructor
 @ToString
 public class Song {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String songName;
     private String album;
     private String author;
